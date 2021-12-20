d4e0cec0dc9bc9211ae424cb749ad60363c23490
Samuel <fifamano@gmail.com>
2021-12-19 21:43:50 -0600

    condiguracion de rutas y hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //rutas que la aplicacion manejara
+  {path:'home',component:PortafolioComponent},
+  {path:'about',component:AboutComponent},
+  {path:'item',component:ItemComponent},
+  {path:'**',pathMatch:'full',redirectTo:'home'}//este es como el defult
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//rutas
 import { AppRoutingModule } from './app-routing.module';
+//componentes creados constantes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+// pages 
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
